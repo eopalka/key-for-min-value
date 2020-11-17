84b7daae632c9bd9da045d24db4ef40ca279090d
elisa <elisaopalka@gmail.com>
2020-11-16 22:19:23 -0500

    commit tree 3fff953e60ba6ba0a6e079e8b29ebce01df2cd6b

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_value = nil
+    min_key = nil
+    name_hash.each do |n, a|
+      if min_value == nil
+        min_value = a
+        min_key = n
+      elsif min_value > a
+        min_value = a
+        min_key = n
+      end
+    end
+    min_key
 end
